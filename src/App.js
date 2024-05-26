769942721292b3b0945971db0d2baed4f570e41c
Gouravchandravanshi <2021et28go@mitsgwl.ac.in>
2024-05-26 17:58:19 +0530

    heaader

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import './components/Footer'
+import Header from './components/Header' 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <div className='bg-red-700 border border-black text-red-950'>me hun ayush malviya</div>
-        </a>
-      </header>
+      <Header></Header>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='flex m-auto my-10 w-4/5 border border-black justify-between'>
+        <div className=''>
+            <img src='./logoev.jpg'></img>
+        </div>
+        <div className='flex-auto w-auto '  >
+            
+            <ul className='flex my-10'>
+            <li >Home</li>
+            <li className='px-5'>Be a Partner</li>
+            <li className='px-5'>Product</li>
+            <li className='px-5'>About Us</li>
+            <li className='px-5'>Contact Us</li>
+            <li >Our Team</li>
+            </ul>
+            
+        </div>
+        <div className='flex-auto w-auto'>
+            <ol>Contact us on 89493849</ol>
+            <div>
+                <img></img>
+                <img></img>
+                <img></img>
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
