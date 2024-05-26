b3eb8542b1da311b04f6a79a0e55cdf422a78fa3
mayushh <ayush.malviya733@gmail.com>
2024-05-26 18:37:41 +0530

    asshu

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+// about the page of the 
+// back ground should be dark purple and the border should be thin black
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>Footer</div>
+  )
+}
+
+export default Footer
