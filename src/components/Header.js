06925f2f1f27f57438a215179b20af6fe6bd6704
mayushh <ayush.malviya733@gmail.com>
2024-05-27 14:13:56 +0530

    navbar css

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,23 +2,23 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <header className='flex m-auto my-10 w-4/5 border border-black justify-between'>
-        <div className=''>
-            <img src='./logoev.jpg'></img>
+    <header className='border-t-green-900 border-t-8 p-3 rounded-b-3xl bg-green-100  text-md font-semibold flex flex-wrap m-auto my-10 w-4/5 border border-black justify-between'>
+        <div className=' my-auto rounded-lg h-4/5 '>
+            <img className='h-20 rounded-lg ' src='https://t3.ftcdn.net/jpg/04/83/17/70/360_F_483177012_cwHfmtTjEMdbu2tbP0go5qA7zYjLlFyQ.jpg'></img>
         </div>
-        <div className='flex-auto w-auto '  >
+        <div className=' my-auto '  >
             
-            <ul className='flex my-10'>
-            <li >Home</li>
-            <li className='px-5'>Be a Partner</li>
-            <li className='px-5'>Product</li>
-            <li className='px-5'>About Us</li>
-            <li className='px-5'>Contact Us</li>
-            <li className='px-5'>team</li>
+            <ul className='hover:cursor-pointer w-auto flex flex-wrap justify-evenly my-auto'>
+            <li className='mx-3 '>Home</li>
+            <li className='mx-3 '>Be a Partner</li>
+            <li className='mx-3'>Product</li>
+            <li className='mx-3'>About Us</li>
+            <li className='mx-3'>Contact Us</li>
+            <li className='mx-3'>team</li>
             </ul>
             
         </div>
-        <div className='flex-auto w-auto'>
+        <div className='my-auto'>
             <ol>Contact us on 89493849</ol>
             <div>
                 <img></img>
