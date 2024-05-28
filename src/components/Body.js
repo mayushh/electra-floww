7593696df082d6ffae9094d9acb1ede6ffb3fc90
mayushh <ayush.malviya733@gmail.com>
2024-05-28 18:17:33 +0530

    working on body

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Body = () => {
   return (
-    <div className=''>
-        <div>
-            <h1 className='pt-[200px] w-8/12 from-neutral-900  text-7xl text-green-700'>Don't Just Charge ELECTRA-FLOW your Vehicle </h1>
-            <h1 className='pt-[200px] w-8/12 from-neutral-900  text-7xl text-green-700'>Don't Just Charge ELECTRA-FLOW your Vehicle </h1>
+    <div className='flex pt-[170px]  justify-around'>
+        <div className='w-8/12  mx-5'>
+            <h1 className=' w-full  font-semibold   text-7xl text-green-700'>Don't Charge ELECTRA-FLOW your Vehicle </h1>
+            <h1 className='text-3xl text-gray-700 my-5 text-left ml-[20px]'>Powering Sustainable Future</h1>
         </div>
-        <div>
-            <img src="" alt="" />
+        <div className='rounded-[100px] mx-5 m-auto border border-black overflow-hidden'> 
+            <img className='w-[500px]  m-auto ' src="https://i.pinimg.com/736x/aa/f3/2c/aaf32c22fcca7dfc71add1597c677bd5.jpg" alt="img" />
         </div>
     </div>
   )
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <header className= 'fixed mx-[120px] border-t-green-900 border-t-12 p-3 rounded-b-3xl bg-green-100  text-md  flex flex-wrap my-10 w-4/5 border border-black justify-between'>
+    <header className= 'shadow-2xl drop-shadow-2xl fixed mx-[30px] border-t-green-900 border-t-12 p-3 rounded-b-3xl bg-green-50  text-md  flex flex-wrap my-6 w-[95%] border border-black justify-between'>
         
         <div className=' my-auto rounded-lg h-4/5 '>
             <img className='h-20 rounded-lg ' src='https://t3.ftcdn.net/jpg/04/83/17/70/360_F_483177012_cwHfmtTjEMdbu2tbP0go5qA7zYjLlFyQ.jpg'></img>
@@ -10,17 +10,17 @@ const Header = () => {
         <div className=' my-auto font-semibold '  >
             
             <ul className='hover:cursor-pointer w-auto flex flex-wrap justify-evenly my-auto'>
-            <li className='mx-3 '>Home</li>
-            <li className='mx-3 '>Be a Partner</li>
-            <li className='mx-3'>Product</li>
-            <li className='mx-3'>About Us</li>
-            <li className='mx-3'>Contact Us</li>
-            <li className='mx-3'>team</li>
+            <li className='mx-3 hover:text-green-900 '>Home</li>
+            <li className='mx-3 hover:text-green-900'>Be a Partner</li>
+            <li className='mx-3 hover:text-green-900'>Product</li>
+            <li className='mx-3 hover:text-green-900'>About Us</li>
+            <li className='mx-3 hover:text-green-900'>Contact Us</li>
+            <li className='mx-3 hover:text-green-900'>team</li>
             </ul>
             
-        </div>
-        <div className='my-auto'>
-            <ol>Contact us on 89493849</ol>
+        </div> 
+        <div className='my-auto font-semibold'>
+            <ol>Call Us: <br /> +91-91327823278</ol>
             <div>
                 <img></img>
                 <img></img>
